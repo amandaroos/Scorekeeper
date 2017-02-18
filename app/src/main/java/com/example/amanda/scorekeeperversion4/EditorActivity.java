7d8708a39936ad40a8dbda68c95f7d3eb28786a1
Amanda Farrell <bluepandadev@gmail.com>
2017-02-17 23:33:53 -0700

    Initial commit - FAB on MainActivity opens EditorActivity

---
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/EditorActivity.java
@@ -0,0 +1,17 @@
+package com.example.amanda.scorekeeperversion4;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Amanda on 2/17/2017.
+ */
+
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
---
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.amanda.scorekeeperversion4;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Log.e("Main Activity OnCreate", "intent created");
+                startActivity(intent);
+            }
+        });
+    }
+}
