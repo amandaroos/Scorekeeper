9e9ad87c560b6c6942e065cdd626af17d9c10c25
Amanda Farrell <bluepandadev@gmail.com>
2017-02-19 00:11:16 -0700

    Created PlayerDbHelper which includes constants for creating and deleting the SQLite database

---
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/data/PlayerDbHelper.java
@@ -0,0 +1,46 @@
+package com.example.amanda.scorekeeperversion4.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.example.amanda.scorekeeperversion4.data.PlayerContract.PlayerEntry;
+
+/**
+ * Using  SQLiteOpenHelper class. When you use this class to obtain references to your database,
+ * the system performs the potentially long-running operations of creating and updating
+ * the database only when needed and not during app startup
+ */
+
+public class PlayerDbHelper extends SQLiteOpenHelper {
+
+    //Database version must be incremented if database schema is changed
+    private static final int DATABASE_VERSION = 1;
+    //Name of the database file
+    private static final String DATABASE_NAME = "scorekeeper.db";
+
+    public PlayerDbHelper (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //deletes old database and schema and builds new database
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
+    }
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + PlayerEntry.TABLE_NAME + " (" +
+                    PlayerEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    PlayerEntry.COLUMN_PLAYER_NAME + " TEXT, " +
+                    PlayerEntry.COLUMN_PLAYER_SCORE+ " INTEGER)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + PlayerEntry.TABLE_NAME;
+}
