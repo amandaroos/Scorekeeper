082f3cc0fbbfa99bf2420bac65b05b4d3dba7ea4
Amanda Farrell <bluepandadev@gmail.com>
2017-02-20 21:44:42 -0700

    Add save button and delete menu option to EditorActivity.
    The delete menu option doesn't do anything when tapped, the save button exits the activity

--- app/src/main/java/com/example/amanda/scorekeeperversion4/EditorActivity.java
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/EditorActivity.java
@@ -1,11 +1,13 @@
 package com.example.amanda.scorekeeperversion4;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
- * Created by Amanda on 2/17/2017.
+ * Allows user to create a new player or edit an existing one.
  */
 
 public class EditorActivity extends AppCompatActivity {
@@ -14,4 +16,32 @@ public class EditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                //TODO Save pet to the database
+                //savePet();
+                //Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                //TODO
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
