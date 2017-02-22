205c67a15453b97fda4930eb8b100af31f62c4e2
Amanda Farrell <bluepandadev@gmail.com>
2017-02-22 16:44:01 -0700

    Setup CursorAdapter in MainActivity

--- app/src/main/java/com/example/amanda/scorekeeperversion4/MainActivity.java
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //The adapter that knows how to create list item views given a cursor
+    PlayerCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Find the ListView which will be populated with the player data
+        ListView playerListView = (ListView) findViewById(R.id.list_view_player);
+
+        //TODO set empty view
+//        //Find and set empty view on the ListView so that it only shows when the list has 0 items.
+//        View emptyView = findViewById(R.id.empty_view);
+//        playerListView.setEmptyView(emptyView);
+
+        //Set up an Adapter to create a list item for each row of pet data in the Cursor
+        //There is no pet data yet (until the loader finishes) so pass in null for the Cursor
+        mCursorAdapter = new PlayerCursorAdapter(this, null);
+        playerListView.setAdapter(mCursorAdapter);
     }
 }
