4f76631b5be348eea9ef522f1bee350befa86275
Amanda Farrell <bluepandadev@gmail.com>
2017-02-22 00:04:48 -0700

    Implement getType(), insert(), update(), and delete() in PlayerProvider

--- app/src/main/java/com/example/amanda/scorekeeperversion4/data/PlayerProvider.java
+++ app/src/main/java/com/example/amanda/scorekeeperversion4/data/PlayerProvider.java
@@ -1,11 +1,15 @@
 package com.example.amanda.scorekeeperversion4.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
+import com.example.amanda.scorekeeperversion4.data.PlayerContract.PlayerEntry;
 
 /**
  * This ContentProvider manages access to the data stored by the app
@@ -19,7 +23,7 @@ public class PlayerProvider extends ContentProvider {
     private static final int PLAYERS = 100;
 
     /** URI matcher code for the content URI for a single player in the players table */
-    private static final int PLAYERS_ID = 101;
+    private static final int PLAYER_ID = 101;
 
     /**
      * UriMatcher object to match a content URI to a corresponding code.
@@ -36,7 +40,8 @@ public class PlayerProvider extends ContentProvider {
         // when a match is found.
 
         sUriMatcher.addURI(PlayerContract.CONTENT_AUTHORITY, PlayerContract.PATH_PLAYERS, PLAYERS);
-        sUriMatcher.addURI(PlayerContract.CONTENT_AUTHORITY, PlayerContract.PATH_PLAYERS + "/#", PLAYERS_ID);
+        sUriMatcher.addURI(PlayerContract.CONTENT_AUTHORITY, PlayerContract.PATH_PLAYERS
+                + "/#", PLAYER_ID);
     }
 
     /**
@@ -55,27 +60,204 @@ public class PlayerProvider extends ContentProvider {
     }
 
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-        return null;
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+
+        //Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        //This cursor will hold the result of the query
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch(match){
+            case PLAYERS:
+                // For the PLAYERS code, query the players table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the pets table.
+                cursor = database.query(PlayerEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case PLAYER_ID:
+                // For the PLAYER_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.scorekeeper/players/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = PlayerEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the players table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(PlayerEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+//        //Set notification URI on the Cursor
+//        //so we know what content URI the Cursor was created for.
+//        //If the the data at this URI changes, then we know we need to update the Cursor.
+//        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PLAYERS:
+                return PlayerEntry.CONTENT_LIST_TYPE;
+            case PLAYER_ID:
+                return PlayerEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PLAYERS:
+                return insertPlayer(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Insert a player into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPlayer(Uri uri, ContentValues values) {
+
+        //TODO remove when default name is available
+        // Check that the name is not null
+        String name = values.getAsString(PlayerEntry.COLUMN_PLAYER_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Player requires a name");
+        }
+
+        //Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = database.insert(PlayerEntry.TABLE_NAME, null, values);
+
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (newRowId == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+//        //Notify all listeners that the data has changed for the pet content URI
+//        //uri: content://com.example.android.pets/pets
+//        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+        return ContentUris.withAppendedId(uri, newRowId);
     }
 
     @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+
+//        //Notify all listeners that the data has changed for the player content URI
+//        //uri: content://com.example.android.pets/pets
+//        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Track the number of rows that were deleted
+        int rowsDeleted;
+
+        switch (match) {
+            case PLAYERS:
+                // Delete all rows that match the selection and selection args
+                rowsDeleted = database.delete(PlayerEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case PLAYER_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PlayerEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(PlayerEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+
+//        // If 1 or more rows were deleted, then notify all listeners that the data at the
+//        // given URI has changed
+//        if (rowsDeleted != 0) {
+//            getContext().getContentResolver().notifyChange(uri, null);
+//        }
+
+        // Return the number of rows deleted
+        return rowsDeleted;
     }
 
     @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
-        return 0;
+    public int update(Uri uri, ContentValues contentValues, String selection,
+                      String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PLAYERS:
+                return updatePlayer(uri, contentValues, selection, selectionArgs);
+            case PLAYER_ID:
+                // For the PLAYER_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = PlayerEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updatePlayer(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Update players in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more players).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updatePlayer(Uri uri, ContentValues values, String selection,
+                             String[] selectionArgs) {
+
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        //TODO remove when default name is available
+        // If name column is present in ContentValues, check that the name is not null
+        if (values.containsKey(PlayerEntry.COLUMN_PLAYER_NAME)) {
+            String name = values.getAsString(PlayerEntry.COLUMN_PLAYER_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("Player requires a name");
+            }
+        }
+
+        //Get writable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new row, returning the primary key value of the new row
+        int rowsUpdated = database.update(PlayerEntry.TABLE_NAME, values, selection, selectionArgs);
+
+//        //Notify all listeners that the data has changed for the pet content URI
+//        //given URI has changed
+//        if (rowsUpdated != 0) {
+//            getContext().getContentResolver().notifyChange(uri, null);
+//        }
+
+        return rowsUpdated;
     }
 }
